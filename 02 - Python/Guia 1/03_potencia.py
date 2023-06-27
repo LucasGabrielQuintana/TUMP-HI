c94ad4dce624939bbdc84e9182f9033cf12d08eb
Lucas Quintana <lucasgabrielquintana@gmail.com>
2023-06-27 09:55:02 -0300

    guia 1 ejercicio 3 python

---
+++ 02 - Python/Guia 1/03_potencia.py
@@ -0,0 +1,9 @@
+# Realizar un algoritmo que permita a un usuario ingresar por teclado la BASE y el EXPONENTE de
+# una potencia y que el resultado sea mostrado por pantalla.
+
+base = int(input("Ingrese la base \n"))
+exponente = int(input("ingrese exponente \n"))
+
+potencia = base**exponente
+
+print("La potencia es ", potencia)
