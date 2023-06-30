40612abc6479ecaf98de5f564cc630584643b4d5
Lucas Quintana <lucasgabrielquintana@gmail.com>
2023-06-30 14:19:29 -0300

    ej1 gui 3

---
+++ 02 - Python/Guia 3/01_num_to_35.py
@@ -0,0 +1,5 @@
+# Realizar un algoritmo que muestre en pantalla los números del 1 al 35 (uno abajo del otro). Utilizar
+# para esto alguna estructura repetitiva.
+
+for i in range(35):
+    print(i+1)
