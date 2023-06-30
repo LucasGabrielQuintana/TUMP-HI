eabe73d85231f64fd7f67aa6c6caee25ba50b0b3
Roberto David Quintana <robertodavidquintana@gmail.com>
2023-06-30 18:41:50 -0300

    C#

---
+++ 03 - C#/Guia 1/01_numeros.cs
@@ -0,0 +1,20 @@
+//Una persona decidi� realizar un algoritmo para mostrar 3 n�meros por pantalla. Decidi� llamar a las
+//		variables num1, num2 y num3 y colocarles los valores 5, 3 y 7. Sin embargo, no sabe de qu� tipos
+//		de datos deber�an ser sus tres variables ni tampoco como asignar dichos valores. Realizar un
+//		algoritmo que declare las variables, les asigne los valores que se necesitan y mostrar por pantalla.//
+
+
+namespace MiPrimerProyecto
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            int c = 7;
+            Console.WriteLine(a);
+        }
+    }
+
+}
